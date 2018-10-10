b1ceaead07eefa589fe1d092fb064ac5ca6743a7
René Blažek <blazek.rene@gmail.com>
2018-10-10 10:36:28 +0200

    ver 3.0.0 credits support

---
+++ src/main/java/sk/trenujme/config/AppConfig.java
@@ -0,0 +1,16 @@
+package sk.trenujme.config;
+
+/**
+ * Created by René on 4.11.2016.
+ */
+
+public class AppConfig {
+
+    /*login configurations*/
+    public static String URL_LOGIN = "http://api.trenujme.sk/account/login/";
+    public static String URL_REGISTER = "http://api.trenujme.sk/account/registration/";
+    public static String URL_LOSTPASSWORD = "http://api.trenujme.sk/account/password/";
+
+
+
+}
